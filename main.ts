d4f604ca04195d94b301886f19655441b24c2771
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-09-20 20:07:47 -0400

    python change 6

--- main.ts
+++ main.ts
@@ -10,4 +10,4 @@ basic.pause(1000)
 
 basic.showString('area of rectangle is 5cm and 3cm A= L x w ' + ('A = 5x3') + ('A=') + (5 * 3).toString() + ('cm^2'))
 
-basic.showString('perimeter of a rectangle is 3cm and 5cm P = 2(l + w)  P = 2*(5+3 = ' + ('p =') + (2*(5 + 3)).toString() + ('cm'))
+basic.showString('perimeter of a rectangle is 3cm and 5cm P = 2(l + w)  P = 2 * (5+3 = ' + ('p =') + (2 * (5 + 3)).toString() + ('cm'))
