8262b5315b7dd0a3e4d8dafaa8c64c33690924d8
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-09-21 00:35:30 +0000

    python failed

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,18 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+from time import sleep
+
+display.clear()
+sleep(1)
 
 
-display.scroll("Hello, World!")
+display.scroll("area of a rectangle is 3cm and 5cm A = 5 x 3= " + str(5*3)() + ('cm^2'))
+
+display.show(Image.happy)
+sleep(0.5)
+display.clear()
+display.scroll("perimeter of a rectangle is 3cm by 5cm P = 2(l+w) + 2(5+3))= " + str (2*(5*3)) + ('cm'))
+display.show(Image.happy)
+sleep(0.5)
+display.clear()
