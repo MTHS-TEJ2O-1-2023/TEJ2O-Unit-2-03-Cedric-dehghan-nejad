d9f8e467a0b3aef30ce65f3316421177d2c9e788
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-09-21 17:28:31 +0000

    python verison 21

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -12,16 +12,16 @@ sleep(1)
 
 
 display.scroll(
-"area of a rectangle is 3cm and 5cm A = 5 x 3= " + str(5*3)() + ('cm^2')
+    "area of a rectangle is 3cm and 5cm A = 5 x 3= " + str(5*3)() + ("cm^2")
 )
 display.show(Image.happy)
 sleep(0.5)
 
-display.clear()
+
 display.scroll(
-"perimeter of a rectangle is 3cm by 5cm P = 2(l+w) + 2(5+3))= "
-+ str (2*(5*3)) 
-+ ('cm')
+    "perimeter of a rectangle is 3cm by 5cm P = 2(l+w) + 2(5+3))= "
+    + str (2*(5*3)) 
+    + ("cm")
 )
 display.show(Image.happy)
 sleep(0.5)
